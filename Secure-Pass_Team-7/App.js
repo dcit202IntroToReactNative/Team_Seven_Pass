435e22d21b3e86157b97d8560252f6e2f679e0f4
BrainyYawson <cadzewoda001@st.ug.edu.gh>
2023-07-15 13:00:04 +0000

    created the start up page

---
+++ Secure-Pass_Team-7/App.js
@@ -0,0 +1,20 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>My first ReactNative group-7 app!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
